4aa440fc72e7d477d01bcffaec79204c10266bd1
Txxicu <105075436+Txxicu@users.noreply.github.com>
2026-09-11 23:08:32 +0300

    Update pack.gd

--- packs/Anime Pack/pack.gd
+++ packs/Anime Pack/pack.gd
@@ -1,20 +1,20 @@
-  extends SoundPack
+extends SoundPack
 
-  func _init() -> void:
-      title = "Anime Pack"
-      author = "Roksiks"
-      sounds = {
-         "good-job": { "name": "Good Job", "file": "good-job.mp3" },
-         "anime-wow-sound-effect": { "name": "Anime WOW", "file": "anime-wow-sound-effect.mp3" },
-         "nya": { "name": "NYA", "file": "nya.mp3" },
-         "oh-my-god-anime": { "name": "Oh my god", "file": "oh-my-god-anime.mp3" },
-         "onniicha": { "name": "Onniichaaa", "file": "onniicha.mp3" },
-         "ryuko-i-got-this_udCWCLO": { "name": "I GOT THIS", "file": "ryuko-i-got-this_udCWCLO.mp3" },
-         "sugoi-sugoi": { "name": "Sugoi Sugoi", "file": "sugoi-sugoi.mp3" },
-         "tole-tole-kawaii": { "name": "Kawaii", "file": "tole-tole-kawaii.mp3" },
-         "tuturu_1": { "name": "Tuturu", "file": "tuturu_1.mp3" },
-         "yamete-kudasai": { "name": "Yamete Kudasai, Ahh", "file": "yamete-kudasai.mp3" },
-         "yandere-laugh": { "name": "Yandere Laugh", "file": "yandere-laugh.mp3" },
-         "za-warudo-stop-time-sound": { "name": "Za Warudo!", "file": "za-warudo-stop-time-sound.mp3" },
-         "nony": { "name": "Nony?", "file": "nony.mp3" },
-  }
+func _init() -> void:
+	title = "Anime Pack"
+	author = "Roksiks"
+	sounds = {
+		"good-job": { "name": "Good Job", "file": "good-job.mp3" },
+		"anime-wow-sound-effect": { "name": "Anime WOW", "file": "anime-wow-sound-effect.mp3" },
+		"nya": { "name": "NYA", "file": "nya.mp3" },
+		"oh-my-god-anime": { "name": "Oh my god", "file": "oh-my-god-anime.mp3" },
+		"onniicha": { "name": "Onniichaaa", "file": "onniicha.mp3" },
+		"ryuko-i-got-this_udCWCLO": { "name": "I GOT THIS", "file": "ryuko-i-got-this_udCWCLO.mp3" },
+		"sugoi-sugoi": { "name": "Sugoi Sugoi", "file": "sugoi-sugoi.mp3" },
+		"tole-tole-kawaii": { "name": "Kawaii", "file": "tole-tole-kawaii.mp3" },
+		"tuturu_1": { "name": "Tuturu", "file": "tuturu_1.mp3" },
+		"yamete-kudasai": { "name": "Yamete Kudasai, Ahh", "file": "yamete-kudasai.mp3" },
+		"yandere-laugh": { "name": "Yandere Laugh", "file": "yandere-laugh.mp3" },
+		"za-warudo-stop-time-sound": { "name": "Za Warudo!", "file": "za-warudo-stop-time-sound.mp3" },
+		"nony": { "name": "Nony?", "file": "nony.mp3" },
+	}
