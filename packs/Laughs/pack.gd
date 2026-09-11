fbf7f8ee1ddc19e5dec1eb183a8dc1be1438e936
Txxicu <105075436+Txxicu@users.noreply.github.com>
2026-09-11 18:26:38 +0300

    Update pack.gd

--- packs/Laughs/pack.gd
+++ packs/Laughs/pack.gd
@@ -3,10 +3,10 @@ extends SoundPack
 func _init() -> void:
 	title = "Laughs"
 	sounds = {
-		"laugh1": { "name": "Laugh 1", "file": "laugh1.mp3" },
-		"laugh2": { "name": "Laugh 2", "file": "laugh2.mp3" },
-		"laugh3": { "name": "Laugh 3", "file": "laugh3.mp3" },
-		"laugh4": { "name": "Laugh 4", "file": "laugh4.mp3" },
-		"laugh5": { "name": "Laugh 5", "file": "laugh5.mp3" },
-		"laugh6": { "name": "Laugh 6", "file": "laugh6.mp3" },
+		"laugh1": { "name": "Laugh Frog", "file": "laugh1.mp3" },
+		"laugh2": { "name": "Laugh Roblox", "file": "laugh2.mp3" },
+		"laugh3": { "name": "Angry Laugh", "file": "laugh3.mp3" },
+		"laugh4": { "name": "Laugh Streamer", "file": "laugh4.mp3" },
+		"laugh5": { "name": "Laugh Streamer 2", "file": "laugh5.mp3" },
+		"laugh6": { "name": "Laugh cunning", "file": "laugh6.mp3" },
 	}
