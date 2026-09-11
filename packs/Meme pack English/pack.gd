8122f23fd10d749bae1f4ec260911181a27639cc
Txxicu <105075436+Txxicu@users.noreply.github.com>
2026-09-11 23:13:18 +0300

    Update pack.gd

--- packs/Meme pack English/pack.gd
+++ packs/Meme pack English/pack.gd
@@ -1,1 +1,11 @@
+extends SoundPack
 
+func _init() -> void:
+	title = "Laughs"
+    author = "Txxicu"
+	sounds = {
+		"anime_ahh": { "name": "Anime ahh", "file": "Anime ahh.mp3" },
+		"bruh": { "name": "Bruh", "file": "Bruh.mp3" },
+		"mi_bombo": { "name": "MI BOMBO", "file": "MI BOMBO.mp3" },
+		"what_the_sigma": { "name": "What the sigma", "file": "What the sigma.mp3" },
+	}
