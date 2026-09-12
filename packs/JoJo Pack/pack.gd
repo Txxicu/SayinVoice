c260ec62b467a3b45d29f8e896dcfdf824e0f83a
Txxicu <105075436+Txxicu@users.noreply.github.com>
2026-09-12 16:35:22 +0300

    Update pack.gd

--- packs/JoJo Pack/pack.gd
+++ packs/JoJo Pack/pack.gd
@@ -1,1 +1,16 @@
+extends SoundPack
 
+func _init() -> void:
+	title = "JoJo Pack"
+    author = "Rab1d"
+	sounds = {
+		"c-moon": { "name": "C-Moon", "file": "c-moon.mp3" },
+		"dojyan": { "name": "Dojyan", "file": "dojyan.mp3" },
+		"goodbye-jojo": { "name": "GoodBye JoJo", "file": "goodbye-jojo.mp3" },
+		"killaqueencatchphrase": { "name": "Bite the dust", "file": "killaqueencatchphrase.mp3" },
+		"made_in_heaven": { "name": "Made in heaven", "file": "made_in_heaven.mp3" },
+		"oh-no": { "name": "Oh no", "file": "oh-no.mp3" },
+    "rero-rero-rero": { "name": "Rero Rero Rero", "file": "rero-rero-rero.mp3" },
+    "star-platinum-za-warudo": { "name": "Za warudo", "file": "star-platinum-za-warudo.mp3" },
+    "chimimi": { "name": "Chimimi", "file": "tusk-chimimi.mp3" },
+	}
